db55ed06bd647dfdd72f61c2224a1cbc2fe5441f
akili <sedov.anton1986@gmail.com>
2022-05-24 22:15:47 +0300

    Запрос рутового пароля в скрипте

--- start_os.sh
+++ start_os.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-apt update && apt install -y python3-pip
+echo "Введите пароль: "
+read -r pass
+
+echo "$pass" | sudo -S apt update && apt install -y python3-pip
+
 python3 -m pip install --user ansible
 
-ansible-playbook make-local.yaml
+ansible-playbook make-local.yaml --extra-vars "ansible_sudo_pass=$pass"
