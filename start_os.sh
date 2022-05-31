cee5218d16f06e92bf47f591a3ef83460994777b
akili <sedov.anton1986@gmail.com>
2022-05-31 21:54:15 +0300

    Проверка наличия папки с конфигами

--- start_os.sh
+++ start_os.sh
@@ -9,7 +9,8 @@ echo "$pass" | sudo -S apt install -y ansible git
 
 cd ~ || exit
 
-git clone https://github.com/akili/configs.git
-cd configs || exit
+CONFIG_DIR='configs'
+[ ! -d "$CONFIG_DIR" ] && git clone https://github.com/akili/configs.git $CONFIG_DIR
+cd $CONFIG_DIR || exit
 
 ansible-playbook make-local.yaml --extra-vars "ansible_sudo_pass=$pass"
