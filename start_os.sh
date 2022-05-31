cd17fba900973e15beec1d749b6cc5befdd4de1c
akili <sedov.anton1986@gmail.com>
2022-05-31 21:26:28 +0300

    Копирование конфигов на новую машину

--- start_os.sh
+++ start_os.sh
@@ -7,4 +7,10 @@ read pass
 echo "$pass" | sudo -S apt update
 echo "$pass" | sudo -S apt install -y ansible
 
+mkdir akili
+cd akili || exit
+
+git clone https://github.com/akili/configs.git
+cd configs || exit
+
 ansible-playbook make-local.yaml --extra-vars "ansible_sudo_pass=$pass"
