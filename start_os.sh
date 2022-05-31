bbc26f3df96d5c677ac2eb32fb3e917cd739ee70
akili <sedov.anton1986@gmail.com>
2022-05-31 22:02:32 +0300

    Обновление репы с конфигамми

--- start_os.sh
+++ start_os.sh
@@ -11,6 +11,6 @@ cd ~ || exit
 
 CONFIG_DIR='configs'
 [ ! -d "$CONFIG_DIR" ] && git clone https://github.com/akili/configs.git $CONFIG_DIR
-cd $CONFIG_DIR || exit
+cd $CONFIG_DIR && git pull
 
 ansible-playbook make-local.yaml --extra-vars "ansible_sudo_pass=$pass"
