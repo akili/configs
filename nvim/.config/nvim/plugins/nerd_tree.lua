1e820b453277e2e86353bb2b560269910c36dbc6
akili <sedov.anton1986@gmail.com>
2023-05-14 18:29:41 +0300

    Хоткей для перехода в nerd_tree на текущий буфер

--- nvim/.config/nvim/plugins/nerd_tree.lua
+++ nvim/.config/nvim/plugins/nerd_tree.lua
@@ -1,6 +1,7 @@
 -- https://github.com/preservim/nerdtree
 
 vim.keymap.set('n', '<leader>n', ':NERDTreeFocus<CR>')
+vim.keymap.set('n', '<C-t>', ':NERDTreeFind<CR>')
 vim.keymap.set('n', '<F2>', ':NERDTreeRefreshRoot<CR> :NERDTreeToggle<CR>')
 vim.g.NERDTreeShowHidden = 1
 
