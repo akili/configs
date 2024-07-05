270d3dcef48c8a0e1f03e7c605fbe90505f34190
akili <sedov.anton1986@gmail.com>
2024-07-05 17:29:07 +0300

    Рефакторинг стартового скрипта

--- configure_os.sh
+++ configure_os.sh
@@ -1,53 +1,68 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo -n "Work(1) or home(2): "
-# shellcheck disable=SC2162
-read locality
+# Prompt for the locality
+read -rp "Work(1) or home(2): " locality
 
+# Update package list and install necessary packages
 sudo apt update
 sudo apt install -y ansible git python3-pip
 
+# Change to home directory
 cd ~ || exit
 
-CONFIG_DIR='configs'
-[ ! -d "$CONFIG_DIR" ] && git clone https://github.com/akili/configs.git $CONFIG_DIR
-cd $CONFIG_DIR && git pull origin master
+# Clone or update the configuration repository
+CONFIG_DIR="$HOME/configs"
+if [ ! -d "$CONFIG_DIR" ]; then
+    git clone https://github.com/akili/configs.git "$CONFIG_DIR"
+else
+    cd "$CONFIG_DIR" || exit
+    git pull origin master
+fi
 
+# Define paths for Ansible files
 VAULT_PASS_FILE=vault.txt
 VARS_FILE=vars.yaml
 
 if [ ! -f $VAULT_PASS_FILE ]; then
-    echo 'Creating file with ansible-vault password. Write pass:'
-    read -sr pass
+    read -sr 'Creating file with ansible-vault password. Write pass: ' pass
     echo "$pass" > $VAULT_PASS_FILE
+    echo
 fi;
 
 if [ ! -f $VARS_FILE ]; then
-    echo 'Creating file with ansible encrypted vars. Write became pass:'
-    read -sr pass
+    read -sr 'Creating file with ansible encrypted vars. Write became pass: ' pass
     echo "ansible_become_pass: $pass" > $VARS_FILE
     ansible-vault encrypt $VARS_FILE
+    echo
 fi;
 
+# Upgrade Ansible
 python3 -m pip install --upgrade --user ansible
 
+# Run Ansible playbooks
 ansible-playbook make-common.yaml -i hosts --vault-password-file=$VAULT_PASS_FILE
 
-if [[ "$locality" == "1" ]]; then
-    ansible-playbook make-work.yaml -i hosts
-    echo "Work apps configurated"
-fi
-if [[ "$locality" == "2" ]]; then
-    ansible-playbook make-home.yaml -i hosts
-    echo "Home apps configurated"
-fi
+case "$locality" in
+    "1")
+        ansible-playbook make-work.yaml -i hosts
+        echo "Work apps configurated"
+        ;;
+    "2")
+        ansible-playbook make-home.yaml -i hosts
+        echo "Home apps configurated"
+        ;;
+    *)
+        echo "Invalid option. Exiting."
+        exit 1
+        ;;
+esac
 
+# Use stow to manage dotfiles
 stow vim
 stow nvim
 stow tmux
 
-echo "Installing and configuration is completed. Reboot the system (y/n)?"
-read -r answer
+read -rp "Installation and configuration are completed. Reboot the system (y/n)?" answer
 if [[ "$answer" == "y" ]]; then
     sudo reboot now
 fi
