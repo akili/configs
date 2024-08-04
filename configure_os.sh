41e809b27f555d5cc4c96811cabbb2fd609abde7
akili <sedov.anton1986@gmail.com>
2024-08-04 09:29:45 +0300

    Supress warning about $PATH

--- configure_os.sh
+++ configure_os.sh
@@ -23,13 +23,13 @@ VAULT_PASS_FILE=vault.txt
 VARS_FILE=vars.yaml
 
  if [ ! -f $VAULT_PASS_FILE ]; then
-    read -p 'Creating file with ansible-vault password. Write pass:' -sr pass
+    read -p 'Creating file with ansible-vault password. Write pass: ' -sr pass
     echo "$pass" > $VAULT_PASS_FILE
     echo
 fi;
 
 if [ ! -f $VARS_FILE ]; then
-    read -p 'Creating file with ansible encrypted vars. Write became pass:' -sr pass
+    read -p 'Creating file with ansible encrypted vars. Write became pass: ' -sr pass
     echo "ansible_become_pass: $pass" > $VARS_FILE
     echo
     ansible-vault encrypt $VARS_FILE
@@ -39,7 +39,7 @@ fi;
 python3 -m pip install --upgrade --user ansible
 
 # Run Ansible playbooks
-ansible-playbook make-common.yaml -i hosts --vault-password-file=$VAULT_PASS_FILE --user $USER
+ansible-playbook make-common.yaml -i hosts --vault-password-file=$VAULT_PASS_FILE --user $USER --no-warn-script-location
 
 case "$locality" in
     "1")
@@ -61,7 +61,7 @@ stow vim
 stow nvim
 stow tmux
 
-read -p "Installation and configuration are completed. Reboot the system (y/n)?" -r answer
+read -p "Installation and configuration are completed. Reboot the system (y/n)? " -r answer
 if [[ "$answer" == "y" ]]; then
     sudo reboot now
 fi
