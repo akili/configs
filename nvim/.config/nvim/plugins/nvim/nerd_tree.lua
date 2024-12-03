235f8bf11f042b59ae325efb8f2180830ec27d4f
akili <sedov.anton1986@gmail.com>
2024-12-03 11:00:05 +0300

    Add NERDTreeIgnore

--- nvim/.config/nvim/plugins/nvim/nerd_tree.lua
+++ nvim/.config/nvim/plugins/nvim/nerd_tree.lua
@@ -4,6 +4,23 @@ vim.keymap.set('n', '<leader>n', ':NERDTreeFocus<CR>')
 vim.keymap.set('n', '<C-t>', ':NERDTreeFind<CR>')
 vim.keymap.set('n', '<F2>', ':NERDTreeRefreshRoot<CR> :NERDTreeToggle<CR>')
 vim.g.NERDTreeShowHidden = 1
+vim.g.NERDTreeIgnore = {
+    '\\.pyc$',
+    '\\.pyo$',
+    '__pycache__',
+    '^venv$',
+    '^env$',
+    '^node_modules$',
+    '^\\.venv$',
+    '^\\.git$',
+    '^\\.mypy_cache$',
+    '^\\.pytest_cache$',
+    '^\\.ruff_cache$',
+    '^\\.tox$',
+    '^\\.idea$',
+    '^\\.vscode$',
+    '^\\.DS_Store$',
+}
 
 -- https://github.com/Xuyuanp/nerdtree-git-plugin
 vim.g.NERDTreeGitStatusUseNerdFonts = 1
