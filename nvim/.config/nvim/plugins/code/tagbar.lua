9b0b88f18fbf1031993b33d9a7ab10dc9a020958
akili <sedov.anton1986@gmail.com>
2024-12-03 11:32:07 +0300

    Change hotkey for TagbarToggle

--- nvim/.config/nvim/plugins/code/tagbar.lua
+++ nvim/.config/nvim/plugins/code/tagbar.lua
@@ -1,6 +1,6 @@
 -- https://github.com/preservim/tagbar
 
-vim.api.nvim_set_keymap('n', '<F8>', ':TagbarToggle<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<F4>', ':TagbarToggle<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>tn', ':TagbarJumpNext<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>tp', ':TagbarJumpPrev<CR>', {noremap = true})
 vim.g.tagbar_compact = 1
