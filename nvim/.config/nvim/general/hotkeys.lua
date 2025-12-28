caffe86e64c59b971fcea6612c458bdf3fc39451
akili <sedov.anton1986@gmail.com>
2025-12-28 09:56:40 +0300

    Add floating terminal

--- nvim/.config/nvim/general/hotkeys.lua
+++ nvim/.config/nvim/general/hotkeys.lua
@@ -11,5 +11,25 @@ keymap("n", "N", "Nzz", default_opts)
 keymap("v", "<", "<gv", default_opts)
 keymap("v", ">", ">gv", default_opts)
 
+-- Terminal inside nvim
 keymap("n", "<leader>t", ":split | terminal<CR>", { silent = true })
 keymap("n", "<leader>T", ":vsplit | terminal<CR>", { silent = true })
+-- Floating terminal
+vim.keymap.set("n", "<leader>ft", function()
+  local buf = vim.api.nvim_create_buf(false, true)
+  local width = math.floor(vim.o.columns * 0.8)
+  local height = math.floor(vim.o.lines * 0.8)
+
+  vim.api.nvim_open_win(buf, true, {
+    relative = "editor",
+    width = width,
+    height = height,
+    col = math.floor((vim.o.columns - width) / 2),
+    row = math.floor((vim.o.lines - height) / 2),
+    style = "minimal",
+    border = "rounded",
+  })
+
+  vim.fn.termopen(vim.o.shell)
+  vim.cmd("startinsert")
+end)
